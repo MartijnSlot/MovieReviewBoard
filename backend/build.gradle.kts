47e4a72824d9bda596eb216d4c8e4cc58f2df574
MartijnSlot <martijn.slot@nedap.com>
2020-03-03 15:05:13 +0100

    movie model + simple gql stuff

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,6 +26,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
+	implementation("com.graphql-java:graphql-spring-boot-starter:5.0.2")
+	implementation("com.graphql-java:graphiql-spring-boot-starter:5.0.2")
+	implementation("com.graphql-java:graphql-java-tools:5.2.4")
 }
 
 tasks.withType<Test> {
---
+++ backend/src/main/kotlin/com/example/MovieReviewBoard/movie/Movie.kt
@@ -0,0 +1,17 @@
+package com.example.MovieReviewBoard.movie
+
+import com.example.MovieReviewBoard.director.Director
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+
+
+@Entity
+data class Movie(
+    var title: String,
+    @ManyToOne var director: Director,
+    var rating: Long,
+    var releaseDate: String,
+    @Id @GeneratedValue var id: Long? = null
+)
