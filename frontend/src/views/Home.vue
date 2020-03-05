cc544fa4625fa6d8333beec3cf7003871961ebd7
MartijnSlot <martijn.slot@nedap.com>
2020-03-05 12:03:48 +0100

    added profile and movieboard, logincomponent en moviecomponent

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,11 +7,13 @@
 
 <script>
 import NavBarComponent from "../components/NavBarComponent.vue";
+import LoginComponent from "../components/LoginComponent.vue";
 
 export default {
   name: "home",
   components: {
-    NavBarComponent
+    NavBarComponent,
+    LoginComponent
   },
   data() {
     return {
---
+++ frontend/src/views/MovieBoard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="movie-board">
+    <NavBarComponent :brand-title="brandTitle" />
+    <div class="card-columns">
+      <MovieComponent v-for="movie in movies" :movie="movie" :key="movie.id" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import NavBarComponent from "../components/NavBarComponent.vue";
+import MovieComponent from "../components/MovieComponent.vue.vue";
+
+export default {
+  name: "movieboard",
+  components: {
+    NavBarComponent,
+    MovieComponent
+  },
+  data() {
+    return {
+      brandTitle: "Movie Review Board"
+    };
+  },
+  computed: {
+    ...mapGetters(["movies", "accessToken"])
+  },
+  created() {
+    if (this.accessToken) {
+      this.$store.dispatch("getMovies", this.accessToken);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card-columns {
+  margin: 3% 2% 3% 2%;
+}
+</style>
